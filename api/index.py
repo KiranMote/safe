daa98a3b5a0d4d477d3292e68e666f949a235a96
kiran mote <kiranm4123@gmail.com>
2025-04-23 23:04:25 +0530

    index.py file corrected

--- api/index.py
+++ api/index.py
@@ -3,6 +3,9 @@ app= Flask(__name__)
 @app.route("/")
 def hello():
     return jsonify(message= "Hello kiran don here!")
+@app.route('favicon.ioc')
+def favicon():
+    return "Kiran don"
 
 def handler(environ, start_response):
     return app(environ, start_response)
