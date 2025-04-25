2f12ac96750b3fd8e3bec31d93a090afe90970b7
kiran mote <kiranm4123@gmail.com>
2025-04-25 11:23:51 +0530

    25 april 2025 11:20AM

--- app.py
+++ app.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def home():
     return render_template('myhtml.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    app.run(debug=True)
