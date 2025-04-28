6f4ebc9819e10d39c95486e8618d8b5a9000c169
kiran mote <kiranm4123@gmail.com>
2025-04-28 12:48:16 +0530

    28 april added bot but need to add api

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from transformers import pipeline
 
 app = Flask(__name__)
 
+# Load the Hugging Face chatbot model
+chatbot = pipeline('text-generation', model='gpt2')
+
 @app.route('/')
 def home():
-    return render_template('myhtml.html')
+    return render_template('index.html')
 
+@app.route('/chat', methods=['POST'])
+def chat():
+    user_input = request.json['message']
+    response = chatbot(user_input, max_length=50)
+    return jsonify({'response': response[0]['generated_text']})
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
