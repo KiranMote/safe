aec97ae6a0b0ad3d6516375bb15178c23456e9e3
kiran mote <kiranm4123@gmail.com>
2025-05-15 19:05:11 +0530

    15 may new app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('deepseek.html')
 
 @app.route('/chat', methods=['POST'])
 def chat():
@@ -13,5 +13,5 @@ def chat():
     response = f"Received: {user_input}"
     return jsonify({'response': response})
 
-#if __name__ == '__main__':
-#   app.run(debug=True)
+if __name__ == '__main__':
+   app.run(debug=True)
